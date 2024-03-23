6591a28df15f5f1aded56c6b985e14fcf402b717
CaptWake <stefanoderosa1999@gmail.com>
2024-03-23 19:54:25 +0100

    Add time utility

--- include/simulation.h
+++ include/simulation.h
@@ -12,10 +12,13 @@ class Simulation {
  public:
   Simulation() {
     std::vector<Body> particles;
+
     particles.emplace_back(1, vec3(-0.97000436, 0.24308753, 0), vec3(0.4662036850, 0.4323657300,0));
     particles.emplace_back(1, vec3(-0, 0, 0), vec3(-0.93240737, -0.86473146,0));
     particles.emplace_back(1, vec3(0.97000436, -0.24308753, 0), vec3(0.4662036850, 0.4323657300,0));
-    this->simulation = std::make_unique<SequentialAP>(SequentialAP(particles, 1));
+
+    //*3600/149597870700
+    this->simulation = std::make_unique<SequentialAP>(SequentialAP(particles, 5.137597588561357e-07));
   }
   void start(double time, double dt) {
     for (double t = 0.0; t < time; t += dt) {
---
+++ include/utilities/time.h
@@ -0,0 +1,16 @@
+#ifndef TIME_H_
+#define TIME_H_
+
+#include <chrono>
+
+#define TIMERSTART(label)                                                  \
+        std::chrono::time_point<std::chrono::system_clock> a##label, b##label; \
+        a##label = std::chrono::system_clock::now();
+
+#define TIMERSTOP(label)                                                   \
+        b##label = std::chrono::system_clock::now();                           \
+        std::chrono::duration<double> delta##label = b##label-a##label;        \
+        std::cout << "# elapsed time ("<< #label <<"): "                       \
+                  << delta##label.count()  << "s" << std::endl;
+
+#endif
