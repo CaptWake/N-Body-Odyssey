fc76a6b1de38744bc614caf815b68563c493d361
CaptWake <stefanoderosa1999@gmail.com>
2024-07-07 22:15:36 +0200

    Remove useless stuff

--- include/simulations/sequential_bh.h
+++ include/simulations/sequential_bh.h
@@ -1,61 +1,41 @@
 #ifndef SEQUENTIAL_BH_H_
 #define SEQUENTIAL_BH_H_
 
-#include <random>
 #include <vector>
+#include "utilities/fileIO.h"
+#include "utilities/vec3.h"
+#include "utilities/nbody_helpers.h"
 
-#include "fileIO.h"
-#include "nbody.h"
-#include "vec3.h"
-
-class SequentialBH : NBody {
+class SequentialBH {
  public:
   SequentialBH() = default;
 
-  SequentialBH(const std::string &fname, const float theta) {
-    std::vector<float> _m, _v, _p;
-    ReadCSVConfigurationAOS(fname, _m, _p, _v, this->G);
-
-    this->m = std::move(_m);
-    this->p.reserve(_p.size());
-    this->v.reserve(_v.size());
-
-    // convert to vec3
-    for (uint64_t i = 0; i < _p.size(); i += 3) {
-      this->p.emplace_back(_p[i], _p[i + 1], _p[i + 2]);
-      this->v.emplace_back(_v[i], _v[i + 1], _v[i + 2]);
-    }
-    this->theta = theta;
-  }
-
   // generate random samples
-  SequentialBH(const uint64_t n_bodies, const float grav_const, float theta) {
-    static std::random_device rd;  // random device engine, usually based on
-                                   // /dev/random on UNIX-like systems
-    // initialize Mersennes' twister using rd to generate the seed
-    static std::mt19937 engine{0};  // rd()};
-    std::uniform_real_distribution<float> density(-1, 1);
-
-    this->G = grav_const;
+  SequentialBH(const uint64_t n_bodies, const float theta) {
     this->theta = theta;
 
-    this->m.reserve(n_bodies);
-    this->p.reserve(n_bodies);
-    this->v.reserve(n_bodies);
+    float *m = new float[n_bodies];
+    float *p = new float[3*n_bodies];
+    float *v = new float[3*n_bodies];
 
-    for (uint64_t i = 0; i < n_bodies; i++) {
-      this->m.push_back(density(engine));
-      this->p.emplace_back(density(engine), density(engine), density(engine));
-      this->v.emplace_back(density(engine), density(engine), density(engine));
+    InitAos<float>(n_bodies, m, p, v);
+
+    for (uint64_t i = 0; i < 3*n_bodies; i+=3) {
+      this->m.push_back(m[i/3]);
+      this->p.emplace_back(p[i], p[i+1], p[i+2]);
+      this->v.emplace_back(v[i], v[i+1], v[i+2]);
     }
+
+    delete[] m;
+    delete[] p;
+    delete[] v;
   }
 
-  void Update(float dt) override;
-  void LogsToCSV(const std::string &filename);
+  void Update(float dt);
 
   std::vector<vec3> p, v;
   std::vector<float> m;
-  float G{}, theta{};
+  float theta{};
 };
 
 #endif
